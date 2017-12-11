23169a692d22c35649adc8180b656baa8f1e3262
Yue Wang <Yue_Wang@student.uml.edu>
2017-12-11 17:23:00 -0500

    intermediate copy

--- Plot/Plot.cpp
+++ Plot/Plot.cpp
@@ -3,10 +3,22 @@
 #include <vector>
 #include <cstdlib>
 #include <fstream>
+#include <thread>
+#include <mutex>
 
 #define NUMOFSEEDS 30
+#define NUMOFTHREADS 900 
 
 using namespace std;
+mutex mtx;
+
+void thread_function (string cmd, unsigned int cnt) {
+	mtx.lock ();
+	system (cmd.c_str ());
+	// cout << cmd << endl;
+	// cout << "The " << cnt << " time" << endl;
+	mtx.unlock ();
+}
 
 int main () {
 	vector<int> Core;
@@ -14,6 +26,8 @@ int main () {
 	static unsigned int NumofRequests = 100000;
 	static string Exec = "~/Desktop/Simulator/SimSDM-EON/Simulator"; 
 	static string Topo = "~/Desktop/Simulator/SimSDM-EON/Topology/NSF14.txt";
+	thread ThreadPointer[900];
+	unsigned int cnt = 0;
 
 	fstream plot ("Plot.txt", fstream::out | fstream::app);
 
@@ -28,12 +42,18 @@ int main () {
 
 	for (unsigned int k = 0; k < Core.size (); k++) {
 		for (unsigned int seed = 0; seed < NUMOFSEEDS; seed++) {
-			for (unsigned int lambda = 500; lambda < 1501; lambda = lambda + 100) {
+			for (unsigned int lambda = 500; lambda < 1401; lambda = lambda + 100) {
 				string Cmd = Exec + ' ' + Topo + ' ' + to_string (NumofRequests) + ' ' + to_string (Core[k]) + ' ' + to_string (lambda * Core[k]) + " 1 " + to_string ((double) rand () / 65535);
 				// cout << Cmd << endl;
-				system (Cmd.c_str ());
+				ThreadPointer[cnt] = thread (thread_function, Cmd, cnt);
+				cnt ++;
+				// system (Cmd.c_str ());
 			}
 		} 
+	}
+
+	for (int i = 0; i < NUMOFTHREADS; i++) {
+		ThreadPointer[i].join ();
 	}
 
 	return 1;
